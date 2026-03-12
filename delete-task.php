489b75d0cd9dd5d9edd08769d6b1045852c9360b
satorime <brix.bitayo@gmai.com>
2026-03-13 04:08:00 +0800

    changes

--- delete-task.php
+++ delete-task.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 include 'connect.php';
 
 $taskName = $_GET['taskName'];
--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
-	include 'connect.php';
+ob_start();
+include 'connect.php';
 ?>
 
 <html lang="en" dir="ltr">
